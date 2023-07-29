a5781dae29c7d32427df541c2d58d4bacbd86e1b
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-29 12:28:41 +0530

    Home page

--- web/views.py
+++ web/views.py
@@ -3,4 +3,7 @@ from django.http.response import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    context={
+        "title" : "Home Page"
+    }
+    return render(request, 'web/index.html',context=context)
