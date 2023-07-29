0f55aa5b546c024883e4b866b628a559289a09ec
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-29 11:58:32 +0530

    User Login

--- users/views.py
+++ users/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.http.response import HttpResponseRedirect
 
 
 def login(request):
-    context={}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request,user)
+                return HttpResponseRedirect("/")
+            
+        context={
+            "title" : "login",
+            "error" :  True,
+            "message" : "Invalid username or password"            
+        }
+        return render(request,"users/login.html",context=context)
+    
+    context={
+        "title" : "login"
+    }
     return render(request,"users/login.html",context=context)
