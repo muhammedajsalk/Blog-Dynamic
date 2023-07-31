4b701e48aa64051e5994fb192856e44782954489
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-31 09:46:25 +0530

    create new post(Model structure)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.OneToOneField("auth.User",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    short_description = models.TextField()
+    description = models.TextField()
+    categories = models.ManyToManyField("posts.Category")
+    time_to_read = models.CharField(max_length=128)
+    featured_image = models.ImageField(upload_to="posts/")
+
+    author = models.ForeignKey("posts.Author",on_delete=models.CASCADE)
+    published_date = models.DateField()
+    is_draft = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
