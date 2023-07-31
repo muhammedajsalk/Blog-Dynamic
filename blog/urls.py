4306579daf32352b767055641a9a545dc46865ff
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-31 09:57:01 +0530

    Create New Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -7,3 +9,10 @@ urlpatterns = [
     path('users/',include("users.urls",namespace="users")),
     path('',include("web.urls",namespace="web")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += (
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
