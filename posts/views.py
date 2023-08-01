2fb2b7b9a29c96fe36f1c7085b9bb0e6e3c504c4
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-01 09:59:46 +0530

    Create New Post(Saving many to many field)

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from posts.forms import PostForm
-from posts.models import Author
+from posts.models import Author,Category
 
 
 @login_required(login_url="/users/login/")
@@ -12,14 +12,23 @@ def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST,request.FILES)
         if form.is_valid():
+
+            tags = form.cleaned_data('tags')
+
             if not Author.objects.filter(user=request.user).exists():
                 author = Author.objects.create(user=request.user,name=request.user.username)
             else:
                 author = request.user.author
+                
             instance = form.save(commit=False)
             instance.published_date = datetime.date.today()
             instance.author = author
             instance.save()
+
+            tags_list =tags.split(",")
+            for tag in tags_list:
+                category, created = Category.objectts.get_or_create(title=tag.strip())
+                instance.categories.add(category)
     else:
         data = {
             "title": "Hello",
