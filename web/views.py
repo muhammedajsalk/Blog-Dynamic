dc27225469448c1ca9b3ea1f74aef04e99de4c05
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-09 12:05:08 +0530

    Home Page(Listing posts)

--- web/views.py
+++ web/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from posts.models import Post
 
 
 def index(request):
+    posts = Post.objects.filter(is_deleted=False,is_draft=False)
     context={
-        "title" : "Home Page"
+        "title" : "Home Page",
+        "posts" : posts
     }
     return render(request, 'web/index.html',context=context)
