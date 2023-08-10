6f4f589902c172b9c07909b988a929932c26b843
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-10 12:29:01 +0530

    Home Page(Pagination view)

--- web/views.py
+++ web/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
+
 from posts.models import Post
 
 
 def index(request):
     posts = Post.objects.filter(is_deleted=False,is_draft=False)
+    
+    instances = Paginator(posts, 9)
+    page = request.GET.get('page', 1)
+
+    try:
+        instances = instances.page(page)
+    except PageNotAnInteger:
+        instances = instances.page(1)
+    except EmptyPage:
+        instances = instances.page(instances.num_pages)
+
+
     context={
         "title" : "Home Page",
-        "posts" : posts
+        "instances" : instances
     }
     return render(request, 'web/index.html',context=context)
