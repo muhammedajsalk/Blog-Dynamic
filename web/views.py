d920a6ea87bcf489aabe4472de236c319d14c09b
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-14 10:51:13 +0530

    Home Page(Filtering With Authors)

--- web/views.py
+++ web/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
 
-from posts.models import Post
+from posts.models import Post,Category,Author
 
 
 def index(request):
     posts = Post.objects.filter(is_deleted=False,is_draft=False)
     
+    categories = Category.objects.all()
+    authors = Author.objects.all()
+
+    search_authors = request.GET.getlist("author")
+    print(search_authors)
+    if search_authors:
+        posts =posts.filter(author__in = search_authors)
+
     instances = Paginator(posts, 2)
     page = request.GET.get('page', 1)
 
@@ -21,6 +29,8 @@ def index(request):
 
     context={
         "title" : "Home Page",
-        "instances" : instances
+        "instances" : instances,
+        "categories" : categories,
+        "authors" : authors
     }
     return render(request, 'web/index.html',context=context)
