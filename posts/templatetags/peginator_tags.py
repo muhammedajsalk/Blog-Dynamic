82a55e6bc774e0c84dfea7383d2551b74c08abd6
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-14 12:31:16 +0530

    Home Page(Update get params to pagination)

--- posts/templatetags/peginator_tags.py
+++ posts/templatetags/peginator_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.core.paginator import Paginator
 
@@ -8,3 +10,19 @@ register = template.Library()
 def get_elided_page_range(instances, number, on_each_side=3, on_ends=2):
     paginator =Paginator(instances.object_list, instances.per_page)
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context,page):
+    request = context['request']
+    full_path = request.get_full_path()
+
+    if "?" in full_path:
+        if "page" in full_path:
+            full_path = re.sub("page=[0-9]+", f"pages={page}",full_path)
+        else:
+            full_path += f"&page={page}"
+    else:
+        full_path += f"?page={page}"
+
+    return full_path
