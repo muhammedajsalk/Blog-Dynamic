b2c602355eefe7b3adf3dfd2aab73bc08631acac
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-30 17:55:53 +0530

    Migrating Database(Creating new postgresql database)

--- blog/settings.py
+++ blog/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "blog",
+        "USER": "postgres",
+        "PASSWORD": "helloworld",
+        "HOST": "localhost",
     }
 }
 
