1f3697855a3b4eeccdbe723ead5decd3c757f2d1
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-30 22:21:25 +0530

    Custom Commands(Removing existing data)

--- posts/management/commands/add_posts.py
+++ posts/management/commands/add_posts.py
@@ -1,9 +1,13 @@
-from typing import Any, Optional
 from django.core.management.base import BaseCommand
+from posts.models import Author,Post,Category
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("Hello")
-        self.stdout.write("hello")
+        print("Deleting existing data........")
+
+        Category.objects.all().delete()
+        Post.objects.all().delete()
+        Author.objects.all().delete()
+        
         
