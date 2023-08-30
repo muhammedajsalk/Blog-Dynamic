d1658bc4008b5c7ab879f68beaade804c16d3ef9
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-08-30 11:45:36 +0530

    My Post Pages(Edit form)

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/',views.create_post, name="create_post"),
     path('my-posts/',views.my_posts, name="my_posts"),
     path('delete/<int:id>/',views.delete_post, name="delete_post"),
+    path('edit/<int:id>/',views.edit_post, name="edit_post"),
     path('draft/<int:id>/',views.draft_post, name="draft_post")
 ]
--- posts/views.py
+++ posts/views.py
@@ -108,3 +108,60 @@ def draft_post(request,id):
     }
 
     return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+
+@login_required(login_url="/users/login/")
+def edit_post(request,id):
+    instance = get_object_or_404(Post,id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+
+            tags = form.cleaned_data['tags']
+
+            if not Author.objects.filter(user=request.user).exists():
+                author = Author.objects.create(user=request.user,name=request.user.username)
+            else:
+                author = request.user.author
+
+            instance = form.save(commit=False)
+            instance.published_date = datetime.date.today()
+            instance.author = author
+            instance.save()
+
+            tags_list =tags.split(",")
+            for tag in tags_list:
+                category, created = Category.objects.get_or_create(title=tag.strip())
+                instance.categories.add(category)
+            
+            response_data = {
+                "title" : "Successfully submitted",
+                "message" : "Successfully submitted",
+                "status" : "success",
+                "redirect" : "yes",
+                "redirect_url" : "/"
+            }
+
+            
+        else:
+            error_message = generate_form_errors(form)
+            print(error_message)
+            response_data = {
+                "title" : "form validation error",
+                "message" : str(error_message),
+                "status" : "error",
+                "stable" : "yes"
+            }
+
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
+    else:
+        category_string = ""
+        for category in instance.categories.all():
+            category_string += f"{category.title},"
+
+        form = PostForm(instance=instance,initial={"tags":category_string[:-1]})
+        context={
+            "title" : "Create New Post",
+            "form" : form,
+        }
+    return render(request, "posts/create.html", context=context)
