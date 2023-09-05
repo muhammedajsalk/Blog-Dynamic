e5fb8e724e0914246d193ce3d2e38aeadd193e84
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-09-05 10:17:10 +0530

    Custom Commands(Collecting data from CSV)

--- posts/management/commands/add_posts.py
+++ posts/management/commands/add_posts.py
@@ -1,4 +1,8 @@
+import csv
+
 from django.core.management.base import BaseCommand
+from django.conf import settings
+
 from posts.models import Author,Post,Category
 
 
@@ -9,5 +13,18 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Post.objects.all().delete()
         Author.objects.all().delete()
-        
+
+        file = open(settings.BASE_DIR / "new_posts.csv")
+        csv_reader = csv.reader(file)
+
+        next(csv_reader)
+
+        for row in csv_reader:
+            author_name = row[1].split(",")[0]
+            content = row[2]
+            date = row[3]
+            tags = row[5]
+            title = row[6]
+
+            print(title)
         
